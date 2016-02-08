6d4865b92e355875b24cd212cdc4e79bcf8117bf
dbaines <dbaines@gmail.com>
2016-02-08 13:01:48 +1030

    Added assets to compile to produciton.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ OrnamentExample::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( styleguide.css modernizr.js selectivizr.js respond.js application_bottom.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
