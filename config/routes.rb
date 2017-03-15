fc1f67af0a8af65269518830ad54bdd743663eb0
dbaines <dbaines@gmail.com>
2017-03-15 16:23:38 +1030

    Re-rengerated ornament

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 OrnamentExample::Application.routes.draw do
 
+  resources :uploads do
+    post :image, on: :collection
+  end
   get '/styleguide/:action' => 'styleguide'
 
   get '/styleguide' => 'styleguide#index'
